732f9ced786affe89d9357f9a5ea91d95212a752
RobbFreitas <robertocompromissos@gmail.com>
2021-05-13 11:37:43 -0300

    up

---
+++ Tipos_de_dados/Tipos_de_dados.js
@@ -0,0 +1,23 @@
+//String
+var text = "Olá Mundo";
+
+console.log(text);
+
+//Number
+
+var num = 1;
+
+console.log(num);
+
+//Boolean: True or False
+
+var a = 2;
+var b = 3;
+var c = 2;
+
+console.log(a==b); //false
+console.log(a==c); //true
+
+
+//Null: representa o vazio
+//Undefined: quando a variável não foi definida
