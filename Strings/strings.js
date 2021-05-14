afba45d19a6b190467cf50b5177d46bc9c3b45cd
RobbFreitas <robertocompromissos@gmail.com>
2021-05-14 20:14:38 -0300

    strings

---
+++ Strings/strings.js
@@ -0,0 +1,12 @@
+// Pode se usar aspas simples e duplas. Aconselhável adotar uma das duas
+// Concatenação
+
+var str = "texto";
+console.log(str); // texto
+
+var concatenar = "texto" + "texto2";
+console.log(concatenar); // textotexto2
+
+var str2 = "texto 'aspas dentro'";
+console.log(str2); // texto 'aspas dentro'
+
