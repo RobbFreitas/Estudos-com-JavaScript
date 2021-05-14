ed9e9316ae0db5dda42f53441a3b1984ea63efa2
RobbFreitas <robertocompromissos@gmail.com>
2021-05-14 19:50:08 -0300

    exercicio operadores comparativos

---
+++ Operadores_comparativos/operadores_comparativos.js
@@ -0,0 +1,24 @@
+// == igual a
+// === valor igual e igual
+// != não é igual
+// !== não igual ou não igual
+// > maior que
+// < menor que
+// >= maior ou igual que
+// <= menor ou igual que
+// Compara um dado com o outro e retorna T ou F (tipo Boolean)
+
+var num1 = 3;
+var num2 = "3";
+
+console.log(num1 == num2); // True
+
+console.log(num1 === num2); // False. Compara também o tipo
+
+console.log (2 != 3); // True
+
+console.log (2 !== '2'); // True
+
+console.log(2 > 3); // False
+
+console.log(2 < 3); // True
