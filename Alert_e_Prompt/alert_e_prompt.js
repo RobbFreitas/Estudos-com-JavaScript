ff021a9063d44d76c1ba43145182264727ed22e0
RobbFreitas <robertocompromissos@gmail.com>
2021-05-14 20:36:42 -0300

    alert e prompt

---
+++ Alert_e_Prompt/alert_e_prompt.js
@@ -0,0 +1,8 @@
+var nome = prompt("Insira seu nome: ")
+var boasvindas = alert("Seja bem-vindo " + nome);
+
+var numero = parseInt(prompt("Digite um número: "));
+var dobro = numero * 2;
+
+alert("O dobro do número " + numero + " é " + dobro);
+alert("Até mais");
