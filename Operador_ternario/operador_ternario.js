05abfdef88f81f7e2fe57f12b900d13226751afe
RobbFreitas <robertocompromissos@gmail.com>
2021-05-16 11:32:48 -0300

    operador ternário

---
+++ Operador_ternario/operador_ternario.js
@@ -0,0 +1,9 @@
+var idade = 18;
+
+(idade >= 18 ? console.log("Maior de idade") : console.log("Menor de idade"));
+
+// Outro exemplo de funcionalidade Pode ter uma variável e dispensar o "console.log"
+
+var idade2 = 17;
+var pode = idade2 >= 18 ? true : false;
+console.log(pode)
