e45df547f165f0c45c07964eb8588a2b6f0ba43e
RobbFreitas <robertocompromissos@gmail.com>
2021-05-16 09:47:03 -0300

    condicionais usando IF

---
+++ Condicionais_IF/condicionais_IF.js
@@ -0,0 +1,7 @@
+var idade = 71;
+
+if (idade >= 18 && idade <= 70) {
+    console.log("Maior de idade: Venda de Bebida Autorizada");
+} else {
+    console.log("Menor de idade ou maior de 70 anos: Venda de Bebida Proibida");
+}
