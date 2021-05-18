494055a8e1c3e1e5d39c8a373097a38319192595
RobbFreitas <robertocompromissos@gmail.com>
2021-05-18 17:10:23 -0300

    Repetição com For e While

---
+++ For_e_While/for_e_while.js
@@ -0,0 +1,18 @@
+// Laço com For
+
+var numero = 5;
+
+for (var i = 0; i < numero; i++) {
+
+    console.log("Executando o for pela " + i + " vez");
+}
+
+// Laço com While
+
+var numero2 = 5;
+
+while (numero2 < 10) {
+
+    console.log("Número " + numero2);
+    numero2++;
+}
