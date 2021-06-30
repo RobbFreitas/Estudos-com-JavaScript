faf13b293848eeb914ed9ee659bca9b68c21c19d
RobbFreitas <robertocompromissos@gmail.com>
2021-06-30 19:09:39 -0300

    delete

--- DOM-events/dom-events.js
+++
@@ -1,8 +0,0 @@
-function h1click(){
-    console.log("Função sendo executada");
-}
-
-function changeH2(input){
-    let h2 = document.getElementsByTagName("h2") [0];
-    h2.innerText = input.value;
-}
