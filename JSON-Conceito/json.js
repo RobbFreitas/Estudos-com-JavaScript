e40cd56f87bfd2fc38a0a018f3e3958cd69f4b95
RobbFreitas <robertocompromissos@gmail.com>
2021-07-10 17:00:13 -0300

    JSON - Exemplos e métodos

---
+++ JSON-Conceito/json.js
@@ -0,0 +1,24 @@
+let aluno = {
+    nome: "Roberto",
+    nota: 8.5
+}
+
+console.log(aluno);
+
+JSON // Representação de um Objeto. Java Script Object Notation
+     // Em aluguns momentos a gente vai precisar gravar um OBJ JS
+     // em algum lugar, por exemplo, mas a gente não vai poder passar
+     // o objeto. A gente vai passar uma Str que representa esse OBJ
+
+// JSON.parse. Pega uma String e tenta transformar num objeto JSON
+// JSON.stringify. Pega um objeto JSON e transforma numa String
+
+let stringify = JSON.stringify(aluno);
+console.log(stringify); // Exibe o "objeto" agora em forma de string normal
+
+let parse = JSON.parse(stringify);
+console.log(parse);
+
+let aluno2 = '{"nome": "José", "nota": 7.8}'; // Essa é a notação para se seguir.
+let paraAluno2 = JSON.parse(aluno2);
+console.log(paraAluno2);
