80aeeb8aa0db92426eead8e802b7060f5e597aad
RobbFreitas <robertocompromissos@gmail.com>
2021-07-09 23:51:57 -0300

    Exemplos de Biblioteca Math

---
+++ Biblioteca-Math/biblioteca-math.js
@@ -0,0 +1,47 @@
+let pi = Math.PI;
+console.log(pi); // 3.1459...
+
+let n = Math.E;
+console.log(n); // 2.71...
+
+let raizQuad = Math.sqrt(16);
+console.log(raizQuad); // 4
+
+let pot = Math.pow(2, 3);
+console.log(pot); // 8
+
+let rand = Math.random(0);
+console.log(rand); // Gera um número entre 0 e 1
+
+let randArrendondado = Math.round(Math.random() * 10);
+console.log(randArrendondado); //  Gera um número aleatório entre 1 e 10
+
+let arredondandoPraBaixo = Math.floor(5.99) // Vai pra 5
+console.log(arredondandoPraBaixo);
+
+let arredondandoPraCima = Math.floor(5.001) // Vai pra 6
+console.log(arredondandoPraCima);
+
+// Math Object Methods
+// Method	Description
+// abs(x)	Returns the absolute value of x
+// acos(x)	Returns the arccosine of x, in radians
+// asin(x)	Returns the arcsine of x, in radians
+// atan(x)	Returns the arctangent of x as a numeric value between -PI/2 and PI/2 radians
+// atan2(y, x)	Returns the arctangent of the quotient of its arguments
+// ceil(x)	Returns the value of x rounded up to its nearest integer
+// cos(x)	Returns the cosine of x (x is in radians)
+// exp(x)	Returns the value of Ex
+// floor(x)	Returns the value of x rounded down to its nearest integer
+// log(x)	Returns the natural logarithm (base E) of x
+// max(x, y, z, ..., n)	Returns the number with the highest value
+// min(x, y, z, ..., n)	Returns the number with the lowest value
+// pow(x, y)	Returns the value of x to the power of y
+// random()	Returns a random number between 0 and 1
+// round(x)	Returns the value of x rounded to its nearest integer
+// sin(x)	Returns the sine of x (x is in radians)
+// sqrt(x)	Returns the square root of x
+// tan(x)	Returns the tangent of an angle
+
+
+
